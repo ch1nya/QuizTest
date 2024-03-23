a6d5d2eca8a85c59938c4d6ae52224805730d6f7
ch1nya <you@example.com>
2024-03-23 16:00:43 +0300

    first commit

---
+++ src/components/MyLoginForm.jsx
@@ -0,0 +1,5 @@
+export function MyLoginForm() {
+    return (
+        <></>
+    )
+}
---
+++ src/components/MyRegistraionForm.jsx
@@ -0,0 +1,68 @@
+import {useEffect, useState} from "react";
+import {Button, Checkbox, Form, Input} from "antd";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
+import {login} from "../api";
+export function MyLoginForm(){
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const onFinish = async (values) => {
+        setLoading(true);
+        try {
+            const response = await login(values.username, values.password);
+            console.log('Login successful:', response);
+            // Дальнейшие действия после успешного входа
+        } catch (error) {
+            setError('Login failed. Please check your credentials.');
+            console.error('Login failed:', error);
+        }
+        setLoading(false);
+    };
+
+    return (
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+        >
+            <Form.Item
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button style={{width:"100%"}} type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+                Or <a href="/registration">register now!</a>
+            </Form.Item>
+
+        </Form>
+    );
+}
+    export default MyLoginForm
